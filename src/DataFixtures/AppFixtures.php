2ac6cc10ab219864faffb1e4ed077ed88656dda2
Papa Matrood <papamatrood@gmail.com>
2021-08-22 08:44:31 +0000

    Les fixtures sont chargés

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use Faker;
+use App\Entity\User;
+use DateTime;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $faker = Faker\Factory::create();
+        $users = [];
+
+        for ($i=0; $i < 50; $i++) { 
+            $user = new User();
+            $user->setUsername($faker->name);
+            $user->setFirstname($faker->firstName());
+            $user->setLastname($faker->lastName());
+            $user->setEmail($faker->email);
+            $user->setPassword($faker->password());
+            $user->setCreatedAt(new DateTime());
+
+            $manager->persist($user);
+
+            $users[] = $user;
+        }
+
+        $categories = [];
+
+        for ($i=0; $i < 15; $i++) { 
+            $category = new Category();
+            $category->setTitle($faker->text(50));
+            $category->setDescription($faker->text(250));
+            $category->setImage($faker->imageUrl());
+
+            $manager->persist($category);
+
+            $categories[] = $category;
+        }
+
+
+        for ($i=0; $i < 15; $i++) { 
+            $article = new Article();
+            $article->setTitle($faker->text(50));
+            $article->setContent($faker->text(6000));
+            $article->setImage($faker->imageUrl());
+            $article->setCreatedAt(new DateTime());
+            $article->addCategory($categories[$faker->numberBetween(0, 14)]);
+            $article->setAuthor($users[$faker->numberBetween(0, 49)]);
+
+            $manager->persist($article);
+        }
+
+        $manager->flush();
+    }
+}
